7e339114c0cef0ad8f769e9ee97d511832884f0e
Mathew <24691566+mesporas1@users.noreply.github.com>
2020-05-27 21:09:45 -0700

    Reorganized files. Added fruit test steps 1 - 7

---
+++ testing-basics-garden-project/test-steps/fruit-test-steps/fruits-test007.js
@@ -0,0 +1,13 @@
+import { FruitTree } from "../src/FruitTree";
+const chai = require("chai");
+const assert = chai.assert;
+
+describe("Fruit Tree Tests", function () {
+    let myFruitTree;
+
+    beforeEach(() => {
+        myFruitTree = new FruitTree("Strawberry");
+    });
+});
+
+// Result from previous above.
